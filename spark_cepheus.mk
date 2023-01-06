8a8383f4be45c36e972c4b139fae3782b03ef914
baalgx <jose@pixelos.net>
2023-01-06 06:34:08 +0000

    cepheus: Stop including the firmware

--- spark_cepheus.mk
+++ spark_cepheus.mk
@@ -41,5 +41,4 @@ PRODUCT_MANUFACTURER := Xiaomi
 # Recovery
 TARGET_USES_AOSP_RECOVERY := true
 
-# Include firmware
-$(call inherit-product, vendor/xiaomi-firmware/cepheus/firmware.mk)
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
