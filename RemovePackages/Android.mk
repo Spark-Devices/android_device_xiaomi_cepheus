72c4e61e929ed49a1f03f59c7931724f2b70f8c4
chematelegram <chfondia@gmail.com>
2023-08-26 23:07:26 +0000

    cepheus: Remove some GApps

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -5,6 +5,7 @@ LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := Maps GoogleFeedback TipsPrebuilt Drive YouTube Videos
+LOCAL_OVERRIDES_PACKAGES := Photos RecorderPrebuilt Chrome
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
